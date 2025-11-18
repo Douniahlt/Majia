c5cb1fcf47fcd18e766e637840439e3b9e3059b8
matea <matea.gabus@gmail.com>
2025-11-18 16:29:54 +0100

    Python: début followTarget et clean

--- Papythons/Python/majia_script_generatePath.py
+++ Papythons/Python/majia_script_generatePath.py
@@ -1,6 +1,8 @@
 import maya.cmds as cmds
 from random import *
 
+# User inputs
+
 animStart = 1
 animEnd = 75
 fps = 24
@@ -15,6 +17,8 @@ sNoiseMax = 7.5
 
 smoothCrv = 10
 sampleCrv = 10
+
+lateToTarget = 5
 
 # Init
 rNbFrames = int((animEnd - animStart) / fps * rNoiseFreq)
@@ -31,7 +35,7 @@ def randomFrameList(frameL, animStart, animEnd, nbFrame):
         frameL.append(frame)
     return frameL
 
-def animRandAtWorldCenter(master, attToNoise):
+def animRandAtWorldCenter(master):
     # animation
     for key in attToNoise:
         (action, attname, nbFrame, Nmin, Nmax) = key
@@ -54,13 +58,79 @@ def animRandAtWorldCenter(master, attToNoise):
         toSmoothL.append(master + "_" + key[1])
     cmds.filterCurve(*toSmoothL, cof=smoothCrv, f="butterworth", sr=sampleCrv)
 
-def main():
+def createMotionAtWorldCenter():
+    # Créer le nombre d'animations choisi par l'utilisateur
     for i in range(nbPaths):
-        master = "master" + str(i+1)
-        child = "placeHolder" + str(i+1)
+        # Générer les noms
+        number = str(i+1)
+        if(len(number) == 1):
+            number = "0" + number
+        master = "master" + number + "_ctrl"
+        child = "placeHolder" + number + "_lctr"
+        # Créer les obj maya
         cmds.spaceLocator(n=child, p=[1, 0, 0])
         cmds.spaceLocator(n=master)
         cmds.parent(child, master)
-        animRandAtWorldCenter(master, attToNoise)
+        # Animation
+        animRandAtWorldCenter(master)
+    # Clean de la scène
+    cmds.select(cmds.ls('master*'))
+    cmds.group(n="root_grp", w=True)
+
+
+
+def listTargetPos(target):
+    posListe = []
+    for frame in range(animStart, animEnd+1):
+        cmds.currentTime(frame)
+        position = cmds.xform(target, q=True, t=True, ws=True)
+        posListe.append(position)
+    return posListe
+
+def followTarget(target):
+    posListe = listTargetPos(target)
+    root = "root_grp"
+    for frame in range(animStart, animEnd+1):
+        cmds.currentTime(frame)
+        idx = frame - animStart
+        cmds.move(*posListe[idx], root)
+        cmds.setKeyframe(root, at="translate")
+
+
+
+
+# Faire des groupes pour les master à la place des locateur
+# Scale le locateur place holder à 1 meme en x
+# Traiter les curves dans follow target pour avoir moins de clef
+# Mettre en place le retard
+# Mettre en place la vitesse et l'accélération du root pour suivre la target
+
+"""
+target = "target_pt"
+posListe = []
+for frame in range(animStart, animEnd+1):
+    cmds.currentTime(frame)
+    position = cmds.xform(target, q=True, t=True, ws=True)
+    posListe.append(position)
+print(posListe)
+
+cmds.curve(p=posListe)
+"""
+
+
+
+
+
+target = "target_pt"
+
+def main():
+    createMotionAtWorldCenter()
+    followTarget(target)
+
 
 main()
+
+
+
+
+
