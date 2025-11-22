c62822b6721c470e648d86e80c3c3004f9576e9b
Ton nom <96797248+Douniahlt@users.noreply.github.com>
2025-11-22 14:08:25 +0100

    fixe vitesse, clé par frame, renommage du fichier

--- ProjetDounia.py
+++ ProjetDounia.py
@@ -379,5 +379,5 @@ def ralentir_pages(frame_debut_ralentissement, duree_ralentissement=30):
 
 creer_pages(nombre_pages=50)
 rigger_pages()
-animer_pages(nombre_pages_a_tourner=20, frame_debut=1, duree_par_page=8, acceleration=True)
+animer_pages(nombre_pages_a_tourner=25, frame_debut=1, duree_par_page=10, acceleration=True)
 ralentir_pages(frame_debut_ralentissement=100, duree_ralentissement=40)
